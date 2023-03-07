4369c5149afa03198577b886a5c4630224e9d10e
unclefish365 <rangahove@gmail.com>
2023-03-07 06:54:47 -0800

    Python If Else Loops Functions

---
+++ 0x01-python-if_else_loops_functions/8-main.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+uppercase = _import_('8-uppercase').uppercase
+
+uppercase("holberton")
+uppercase("Holberton School 98 Battery street")
---
+++ 0x01-python-if_else_loops_functions/9-main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+print_last_digit = _import_('9-print_last_digit').print_last_digit
+
+print_last_digit(98)
+print_last_digit(0)
+r = print_last_digit(-1024)
+print(r)
